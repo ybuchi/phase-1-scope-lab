767d980fb20ba7f3b0be0ea80e64f2f2739a310f
ybuchi <yabuch_ch@hotmail.com>
2022-02-23 16:30:47 -0500

    commit tree 6b0865eebc58f06b47263bec7b5184423ee2d5a5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "";
+
+function upperCaseCustomerName(name){
+    customerName = name.toUpperCase();
+};
+
+function setBestCustomer(){
+    var bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "something else";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "John";
+}
+
+console.log(upperCaseCustomerName);
+console.log(setBestCustomer);
+
